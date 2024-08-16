2adc60ee1cc0e7b0eb5194df9e929a3032135144
Lautaro Jimenez <lauta.jimenez@gmail.com>
2024-08-16 20:19:26 -0300

    Login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AuthForm from './components/AuthForm.jsx';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <AuthForm></AuthForm>     
+
   );
 }
 
