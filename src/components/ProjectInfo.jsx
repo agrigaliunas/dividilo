ad768dcba012e9b18ccf664bf7458dba852b3497
Lautaro <lauta.jimenez@gmail.com>
2024-09-23 18:37:34 -0300

    commit

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -20,11 +20,24 @@ TODO:
 
 export const ProjectInfo = ({ project }) => {
   const [projectNombre, setProjectNombre] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [projectMonto, setProjectMonto] = useState("");
   const [disableEditNombre, setDisableEditNombre] = useState(true);
+  const [editandoProyecto, setEditandoProyecto] = useState(null);
+
 
   useEffect(() => {
     setProjectNombre(project.nombre);
   }, [project.nombre]);
+
+  useEffect(() => {
+    setProjectDescription(project.descripcion);
+  }, [project.descripcion]);
+
+  useEffect(() => {
+    setProjectMonto(project.monto);
+  }, [project.monto]);
+
 
   const handleEditNombre = (e) => {
     setProjectNombre(e.target.value);
@@ -59,31 +72,55 @@ export const ProjectInfo = ({ project }) => {
     }
   };
 
+  const handleEditarClick = (project) => {
+    setEditandoProyecto(project);
+  };
+
   return (
     <div className="bg-white w-[80vw] p-4 rounded-xl flex flex-col gap-5">
       <div className="flex flex-col gap-3 justify-center">
-        <div className="flex flex-row gap-2 items-center p-1">
-          <input
-            disabled={disableEditNombre}
-            onChange={handleEditNombre}
-            className="text-2xl font-semibold border-b border-gray-400 p-1"
-            value={projectNombre}
-            placeholder={projectNombre}
-          />
-          <button
-            onClick={handleNameClick}
-            className={`rounded-md p-1 ${
-              disableEditNombre ? "bg-gray-200" : "bg-green-200"
-            } hover:opacity-80 flex items-center justify-center gap-2`}
-          >
-            <span className="text-xs">
-              {!disableEditNombre ? "Guardar" : "Editar"}
+        {editandoProyecto ? (
+          <>
+            <div className="flex flex-row gap-2 items-center justify-center p-1">
+              <input
+                disabled={disableEditNombre}
+                onChange={handleEditNombre}
+                className="text-4xl font-semibold bg-white text-center border-b-2"
+                value={projectNombre}
+                placeholder={projectNombre} />
+            </div>
+            <span>Descripción:
+              <input
+                className="text-lg border-b-2"
+                value={projectDescription}
+              />
             </span>
-            {disableEditNombre ? <EditIcon /> : <PaperAirplane />}
-          </button>
-        </div>
-        <span className="text-lg">{project.descripcion}</span>
+            <span>
+              Monto:
+              <input
+                value={projectMonto}
+              />
+            </span>
+          </>
+
+        ) : (
+          <>
+            <span className="text-4xl font-semibold text-center">{projectNombre}</span>
+            <span className="text-lg">Descripción: {projectDescription}</span>
+          </>
+        )}
+        <button
+          onClick={handleEditarClick}
+          className={`rounded-md p-1 ${disableEditNombre ? "bg-gray-200" : "bg-green-200"
+            } hover:opacity-80 flex items-center justify-center gap-2`}
+        >
+          <span className="text-xs">
+            {!disableEditNombre ? "Guardar" : "Editar"}
+          </span>
+          {disableEditNombre ? <EditIcon /> : <PaperAirplane />}
+        </button>
       </div>
-    </div>
+    </div >
+
   );
 };
