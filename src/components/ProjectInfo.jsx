37f553793be77be4f94de711baed539e9e63c9ba
Lautaro <lauta.jimenez@gmail.com>
2024-09-23 23:12:27 -0300

    Proyecto page

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -10,7 +10,8 @@ export const ProjectInfo = ({ project, usuarios }) => {
     descripcion: "",
     montoTotal: "",
     fecha: "",
-    participantes: []
+    participantes: [],
+    gastos: []
   });
 
   const [editandoProyecto, setEditandoProyecto] = useState(false);
@@ -34,6 +35,26 @@ export const ProjectInfo = ({ project, usuarios }) => {
     }
     return "Desconocido";
   };
+
+  const getGastosInfo = (g) => {
+    if (project.gastos) {
+      return project.gasto && `${project.gasto.descripcion}`
+    }
+    return "Desconocido";
+  };
+
+  const getIniciales = (participanteId) => {
+    if (usuarios) {
+      const usuario = usuarios.find(user => user.id === participanteId);
+      if (usuario) {
+        const inicialNombre = usuario.nombre.charAt(0).toUpperCase();
+        const inicialApellido = usuario.apellido.charAt(0).toUpperCase();
+        return `${inicialNombre}${inicialApellido}`;
+      }
+    }
+    return "??";
+  };
+
 
   // Actualiza el nombre del proyecto en el estado
   const handleEditNombre = (e) => {
@@ -61,9 +82,74 @@ export const ProjectInfo = ({ project, usuarios }) => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col w-full lg:px-[20vw] lg:py-[5vw] h-svh px-[5vw] py-[5vw] gap-10">
 
-      
+      <button className="flex flex-row bg-brandblue rounded-xl border border-1 w-fit p-3 gap-2">
+        <span className="text-white">Editar proyecto</span>
+        <span className="text-white "><EditIcon /></span>
+      </button>
+
+      <div className="flex flex-col gap-5 border border-1 bg-white p-5 rounded-xl shadow-md">
+        <h1 className="text-4xl text-left font-semibold">{project.nombre}</h1>
+        <span>{project.descripcion}</span>
+      </div>
+
+      <div className="flex flex-row gap-4">
+
+        <div className="flex flex-col border border-1 bg-white rounded-xl shadow-md p-5 w-[50%]">
+          <h1 className="text-2xl text-left font-semibold">Monto total</h1>
+          <div className="flex-grow"></div>
+          <span className="text-4xl font-bold mt-4">$ {project.montoTotal}</span>
+        </div>
+
+
+        <div className="border border-1 bg-white rounded-xl shadow-md p-5 w-[50%]">
+          <div className="flex flex-col gap-5">
+            <h1 className="text-2xl text-left font-semibold">Participantes</h1>
+            <span className="text-4xl font-bold">{project.participantes?.length}</span>
+            <div className="flex flex-row gap-4">
+              {project.participantes?.length > 0 ? (
+                project.participantes.map((p, index) => (
+                  <span className="rounded-full border border-1 border-gray-400 bg-brandblue text-white font-semibold p-2">{getIniciales(p)}</span>
+                ))
+              ) : (
+                <span>No hay participantes</span>
+              )}
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="flex flex-col gap-5 border border-1 bg-white p-5 rounded-xl shadow-md">
+        <h1 className="text-4xl text-left font-semibold">Gastos de los participantes</h1>
+        <div className="flex flex-col gap-4">
+          {project.participantes?.length > 0 ? (
+            project.participantes.map((p, index) => (
+              <span className="">{getParticipanteInfo(p)}</span>
+            ))
+          ) : (
+            <span>No hay participantes</span>
+          )}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5 border border-1 bg-white p-5 rounded-xl shadow-md">
+        <h1 className="text-4xl text-left font-semibold">Gastos</h1>
+        <div className="flex flex-col gap-4">
+          {project.gastos?.length > 0 ? (
+            project.gastos.map((gasto, index) => (
+              <div className="flex flex-row justify-items-stretch">
+                <span className="font-semibold">{gasto.descripcion}</span>
+                <span className="ml-auto">${gasto.monto}</span>
+              </div>
+
+            ))
+          ) : (
+            <span>No hay gastos</span>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
